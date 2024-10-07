b794f748e627e74c68a199887458a2cf9d2416d7
Gerardo <gerardo.dev21@hotmail.com>
2024-10-06 22:09:58 -0600

    Implementación de función ReturnTotalMiliseconds(), funciona correctamente pero es posible realizar una optimización en el algoritmo

--- easy/8-ConversorTiempo/src/ConversorTiempo.java
+++ easy/8-ConversorTiempo/src/ConversorTiempo.java
@@ -3,29 +3,36 @@ import java.util.Scanner;
 public class ConversorTiempo {
 
     //Función que retorna el total de milisegundos
+    public static long ReturnTotalMilisecond(int seconds, int minutes, int hours, int days){
+        long totalMiliseconds = 0L;
+        int milisecondsInSecond = 1000;
+        int milisecondsInMinute, milisecondsInHour, milisecondsInDay;
+
+        //Calculo de milisegundos en segundos, días, horas, días
+        milisecondsInSecond *= seconds;
+        milisecondsInMinute = (1000)*(60*minutes);
+        milisecondsInHour = (1000)*(3600*hours);
+        milisecondsInDay = (1000)*(86400*days);
+        totalMiliseconds = (milisecondsInSecond+milisecondsInMinute+milisecondsInHour+milisecondsInDay);
+
+        return totalMiliseconds;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int days, hours, minutes, seconds, miliseconds = 0;
-        int milisecondsInSecond = 1000;
-        int milisecondsInHour = 3600000;
-        int milisecondsInDays = 3600000;
-        int milisecondsInMinute;
+        int days, hours, minutes, seconds;
+        long totalMiliseconds;
 
         System.out.print("Ingresa segundos: ");
         seconds = scanner.nextInt();
         System.out.print("Ingresa minutos: ");
         minutes = scanner.nextInt();
-//        System.out.print("Ingresa horas: ");
-//        hours = scanner.nextInt();
-//        System.out.print("Ingresa días: ");
-//        days = scanner.nextInt();
+        System.out.print("Ingresa horas: ");
+        hours = scanner.nextInt();
+        System.out.print("Ingresa días: ");
+        days = scanner.nextInt();
 
-        milisecondsInSecond = (milisecondsInSecond)*seconds;
-        milisecondsInMinute = (milisecondsInSecond)*(60*minutes);
-//        milisecondsInDays = (milisecondsInHour)*24;
-
-        System.out.println("milisecondsInSecond = " + milisecondsInSecond);
-        System.out.println("milisecondsInMinute = " + milisecondsInMinute);
+        totalMiliseconds = ReturnTotalMilisecond(seconds, minutes, hours, days);
+        System.out.println("totalMiliseconds = " + totalMiliseconds);
     }
 }
