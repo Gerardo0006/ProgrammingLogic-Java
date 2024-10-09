7a5d96409703ea7f6d7502318f0880a65b58bacc
Gerardo <gerardo.dev21@hotmail.com>
2024-10-08 21:41:42 -0600

    Refactor en Ejercicio 9 - Conjuntos. Aún no funciona correctamente, imrpime los valores que coinciden en ambos arrays pero además imprime basura

---
+++ easy/9-Conjuntos/src/Conjuntos.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Conjuntos {
+
+    public static int[] RetornaArray(int[] array1, int[] array2, boolean bool){
+        int cont = 0;
+        int[] arrayFinal = new int[cont];
+        if (bool){
+            int index = 0;
+            for (int i=0; i<array1.length; i++){
+                for (int j=0; j<array2.length; j++){
+                    if (array1[i] == array2[j]){
+                        arrayFinal[index] = array1[i];
+                        index++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //Imprimimos el arrayFinal
+        for (int index=0; index<arrayFinal.length; index++){
+            System.out.print("[" + arrayFinal[index] + "]");
+        }
+
+
+        return arrayFinal;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 3, 5, 7, 9};
+        int[] finalArray = new int[];
+        int decision;
+        boolean bool = false;
+
+        //Algoritmo para contar el núemro de coincidencias
+
+        do {
+            System.out.println("Seleccione el valor de bool");
+            System.out.println("Verdadero ------------ 1");
+            System.out.println("Falso ---------------- 2");
+            System.out.print("Ingrese su selección: ");
+            decision = scanner.nextInt();
+
+            if (decision == 1){
+                bool = true;
+                System.out.println("bool = " + bool);
+                break;
+            }
+            if (decision == 2){
+                bool = false;
+                System.out.println("bool = " + bool);
+                break;
+            } else if (decision != 1 && decision != 2){
+                System.out.println("Opción incorrecta, intente de nuevo");
+            }
+        }while (decision != 1 || decision != 2);
+
+        finalArray = RetornaArray(array1, array2, bool);
+        System.out.println("finalArray = " + finalArray);
+    }
+}
